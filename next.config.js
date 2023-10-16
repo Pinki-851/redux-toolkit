b7aa9f91a4c5cbe11a3c28085ee24141223f4457
Pinki-851 <pinkisaroj942@gmail.com>
2023-10-16 09:30:52 +0530

    feat: next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   images: {
-    domains: ["m.media-amazon.com"], // Add the domain(s) you want to allow here
+    domains: ["m.media-amazon.com"],
+    // Add the domain(s) you want to allow here
+    distDir: isProd ? `/${process.env.DIST_DIR}` : ".next",
   },
 };
 
