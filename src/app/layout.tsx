f0434edd88615519c97dd60a8e2a6e8576f9bcc1
Pinki Saroj <pinki.saroj@midasfintechsolutions.com>
2024-04-26 17:03:32 +0530

    ads added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
+      <head>
+        <script
+          async
+          src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1426495303323810'
+          crossOrigin='anonymous'
+        ></script>
+      </head>
       <body className={`${inter.className} no-scrollbar bg-primarycolor`}>
         <ReduxProvider>
           <Fragment>
